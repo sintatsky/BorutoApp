08f151d1e2dcba7ab2319dd08a3852dea83ae5fa
Ilia Kreslov <kreslov@telros.ru>
2022-04-26 17:56:14 +0300

    upd: setup navigation

--- app/src/main/java/com/example/borutoapp/MainActivity.kt
+++ app/src/main/java/com/example/borutoapp/MainActivity.kt
@@ -10,13 +10,21 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.borutoapp.navigation.SetupNavGraph
 import com.example.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navHostController = navController)
             }
         }
     }
