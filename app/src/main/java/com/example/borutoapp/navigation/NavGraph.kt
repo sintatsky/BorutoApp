75290fc380191c8e1a9cbb4e480cc1b27ba76bb6
Ilia Kreslov <kreslov@telros.ru>
2022-04-27 19:03:00 +0300

    upd: design WelcomeScreen 2

--- app/src/main/java/com/example/borutoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/borutoapp/navigation/NavGraph.kt
@@ -7,19 +7,22 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.borutoapp.screens.splash.SplashScreen
+import com.example.borutoapp.screens.welcome.WelcomeScreen
 import com.example.borutoapp.util.Constants.DETAILS_ARGUMENT_KEY
+import com.google.accompanist.pager.ExperimentalPagerApi
 
+@ExperimentalPagerApi
 @Composable
 fun SetupNavGraph(navHostController: NavHostController) {
     NavHost(
         navController = navHostController,
-        startDestination = Screen.Splash.route
+        startDestination = Screen.Welcome.route
     ) {
         composable(route = Screen.Splash.route) {
             SplashScreen(navHostController = navHostController)
         }
         composable(route = Screen.Welcome.route) {
-
+            WelcomeScreen(navHostController = navHostController)
         }
         composable(route = Screen.Home.route) {
 
