ace91232749779bf0809d42a3fbebba0de4ebfd2
Ilia Kreslov <kreslov@telros.ru>
2022-04-27 12:53:56 +0300

    upd: add RemoteKey & DatabaseConverter

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -3,4 +3,5 @@ package com.example.borutoapp.util
 object Constants {
     const val DETAILS_ARGUMENT_KEY = "heroId"
     const val HERO_DATABASE_TABLE = "hero_table"
+    const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
 }
