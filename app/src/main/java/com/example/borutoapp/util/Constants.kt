acf945da815f964069671ab955ca0e7729061626
Ilia Kreslov <kreslov@telros.ru>
2022-04-28 17:58:50 +0300

    upd: create NetworkModule

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -1,6 +1,9 @@
 package com.example.borutoapp.util
 
 object Constants {
+
+    const val BASE_URL = "http://10.0.2.2:8080"
+
     const val DETAILS_ARGUMENT_KEY = "heroId"
     const val HERO_DATABASE_TABLE = "hero_table"
     const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
